9b74f3698a9cdc385bbfb155beff2f9ba2f5b159
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-04-24 10:05:17 +0530

    Best Time to Buy and Sell Stock II

---
+++ BestTimetoBuyandSellStockII.py
@@ -0,0 +1,15 @@
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Total profit is 4 + 3 = 7.
+
+def maxProfit(prices):
+    total_profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            total_profit += prices[i] - prices[i - 1]
+    return total_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit(prices))  
