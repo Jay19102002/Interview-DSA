4eb18c9db806bfb028ca6d4136b2218b72b57146
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-04-27 10:09:36 +0530

    Single Number

---
+++ SingleNumber.py
@@ -0,0 +1,12 @@
+# Input: nums = [4,1,2,1,2]
+
+# Output: 4
+
+def singleNumber(nums):
+    result = 0
+    for num in nums:
+        result ^= num
+    return result
+
+nums = [4, 1, 2, 1, 2]
+print(singleNumber(nums)) 
